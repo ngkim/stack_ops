c3e1855043d650f157606b8afbca409c41afacb7
Namgon Kim <day10000@gmail.com>
2015-04-20 13:30:08 +0900

    change python code to get geo location from ip make watch_conntrack.sh to show tcp only

--- geo_from_ip/get_loc_from_ip.py
+++ geo_from_ip/get_loc_from_ip.py
@@ -1,13 +1,24 @@
+#!/usr/bin/python
+
+import sys
 import pygeoip
 
-gi = pygeoip.GeoIP('/root/GeoLiteCity.dat')
+gi = pygeoip.GeoIP('GeoLiteCity.dat')
 
 def printRecord(tgt):
     rec = gi.record_by_name(tgt)
     city = rec['city']
     country = rec['country_name']
-    print '[*] Target: ' + tgt + ' Geo-located. '
-    print '[+] ' + str(country)
+    #print '[*] Target: ' + tgt + ' Geo-located. '
+    #print '[+] ' + str(country)
+    print str(country)
 
-tgt = "220.123.31.119"
-printRecord(tgt)
+
+def main(argv):
+    tgt = "220.123.31.119"
+    tgt = argv[0]
+    printRecord(tgt)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])

a986fc27b61485e291b999974edaa78e7ac8d2f1
root <root@manager.(none)>
2015-10-06 19:39:09 +0900

    ifstat-max.sh - write to file if filename is given

--- pktstat/ifstat-max.sh
+++ pktstat/ifstat-max.sh
@@ -7,8 +7,8 @@
 ########################################
 
 usage() {
-  echo "Usage: $0 [DEV]"
-  echo "   ex)$0 eth0"
+  echo "Usage: $0 [DEV] [FILENAME]"
+  echo "   ex)$0 eth0 eth0.dump"
   exit 1
 }
 
@@ -17,8 +17,9 @@ if [ -z $1 ]; then
 fi
 
 DEV=$1
+FILENAME=$2
 
-ifstat -b -i $DEV -n | awk '
+ifstat -b -i $DEV -n | awk -v dump=$FILENAME '
 /BEGIN/ {
   max_rx=0.0
   max_tx=0.0
@@ -29,6 +30,10 @@ ifstat -b -i $DEV -n | awk '
     str_now=strftime("%Y-%m-%d %H:%M:%S", now);
     if (max_rx < $1 ) { max_rx = $1 }
     if (max_tx < $2 ) { max_tx = $2 }
-    printf("%-20s rx= %10.2f tx= %10.2f max_rx= %10.2f max_tx= %10.2f\n", str_now, $1, $2, max_rx, max_tx);
+    if ( dump != "" ) {
+      printf("%-20s rx= %10.2f tx= %10.2f max_rx= %10.2f max_tx= %10.2f\n", str_now, $1, $2, max_rx, max_tx) | "tee "dump
+    } else {
+      printf("%-20s rx= %10.2f tx= %10.2f max_rx= %10.2f max_tx= %10.2f\n", str_now, $1, $2, max_rx, max_tx)
+    }
   } 
-}'
+}' 

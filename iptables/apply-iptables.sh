6b218b2ccd6fdaa1e3404d5fbb52501d13121c7b
Namgon Kim <day10000@gmail.com>
2015-10-16 11:17:55 +0900

    script to backup and apply new iptables rule

---
+++ iptables/apply-iptables.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+run_command() {
+  local CMD=$*
+  
+  echo $CMD
+  eval $CMD
+}
+
+BAK_DIR="00-iptables.up.rules/${HOSTNAME}"
+BAK_DATE=`date +%y%m%d-%H%M`
+
+mkdir -p $BAK_DIR
+cmd="cp /etc/iptables.up.rules $BAK_DIR/iptables.up.rules-${BAK_DATE}"
+run_command $cmd
+
+cmd="cp iptables.up.rules /etc"
+run_command $cmd
+
+cmd="iptables-restore < /etc/iptables.up.rules"
+run_command $cmd

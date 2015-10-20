8202d68960c8a1ace29182119baf38857fb88335
Namgon Kim <day10000@gmail.com>
2015-10-20 10:36:21 +0900

    Add comment to apply-iptables Add iptables.up.rules-m-node including NAT rules (iptables -L -n -t nat)

--- iptables/apply-iptables.sh
+++ iptables/apply-iptables.sh
@@ -7,17 +7,36 @@ run_command() {
   eval $CMD
 }
 
+#----------------------------------------------------------
+# NGKIM: 2015-10-20
+# use first argument as iptables script file name
+#----------------------------------------------------------
+if [ -z $1 ]; then
+  IPTBL_FILENAME="iptables.up.rules"
+else
+  IPTBL_FILENAME=$1
+fi
+
 BAK_DIR="00-iptables.up.rules/${HOSTNAME}"
 BAK_DATE=`date +%y%m%d-%H%M`
 
+#----------------------------------------------------------
+# step 1. backup /etc/iptables.up.rules
+#----------------------------------------------------------
 mkdir -p $BAK_DIR
 if [ -f /etc/iptables.up.rules ]; then
   cmd="cp /etc/iptables.up.rules $BAK_DIR/iptables.up.rules-${BAK_DATE}"
   run_command $cmd
 fi
 
-cmd="cp iptables.up.rules /etc"
+#----------------------------------------------------------
+# step 2. copy iptables script to /etc
+#----------------------------------------------------------
+cmd="cp ${IPTBL_FILENAME} /etc/iptables.up.rules"
 run_command $cmd
 
+#----------------------------------------------------------
+# step 3. run iptables-restore
+#----------------------------------------------------------
 cmd="iptables-restore < /etc/iptables.up.rules"
 run_command $cmd
